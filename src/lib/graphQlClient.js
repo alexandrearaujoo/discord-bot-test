1606cfa64fd43bfaf1cd9a54b95a375ba2bcd275
Alexandre <alexandre01393@gmail.com>
2022-11-11 13:35:34 -0300

    fix: removing api url and apikey

--- src/lib/graphQlClient.js
+++ src/lib/graphQlClient.js
@@ -3,10 +3,10 @@ const fetch = require("node-fetch");
 
 const graphQlClient = new ApolloClient({
   link: new HttpLink({
-    uri: "https://lizvrgxyxanjqyltiyyy.supabase.co/graphql/v1",
+    uri: process.env.API_URL,
     fetch,
     headers: {
-      apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpenZyZ3h5eGFuanF5bHRpeXl5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2Njk4MzcxOCwiZXhwIjoxOTgyNTU5NzE4fQ.iKEBWN7ZFU0wZN2q9CDD1LxnRLQ6OxvUehz3wg2ZnAs",
+      apiKey: process.env.API_KEY 
     },
   }),
   cache: new InMemoryCache(),
